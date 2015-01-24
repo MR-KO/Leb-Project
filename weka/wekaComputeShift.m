3aaa83e60dd35afa49340fce113ea31dc33d103c
Sjoerd Bouber <sjoerdbouber@hotmail.com>
2015-01-24 14:55:15 +0100

    Allow feature subset

--- weka/wekaComputeShift.m
+++ weka/wekaComputeShift.m
@@ -1,10 +1,14 @@
 % Computes estimated clock shift by repeatedly shifting one interval to the left
 % until classified as 'not shifted'
 
+%function shift = wekaComputeShift(dataset, classifier, feature_subset, test_id)
+
+
 % TODO make this a function
-DATASET = 'datasets/data_10_avg.csv';
+DATASET = 'datasets/data_6_avg.csv';
 CLASSIFIER = 'trees.RandomForest';
-TEST_ID = 24;
+FEATURES = [];
+TEST_ID = 17;
 
 % Add weka and svm libraries to the classpath
 if ~wekaPathCheck
@@ -19,6 +23,12 @@ display(data(TEST_ID, 1:7));
 
 % Remove date/time etc. and set last col to class (col index 7)
 features = data(:, 8:end);
+no_features = size(features, 2);
+% Apply feature subset
+if isempty(FEATURES)
+    FEATURES = 1:length(features); 
+end
+features_subset = features(:, FEATURES);
 classes = data(:, 7);
 test_class = classes(TEST_ID, 1);
 
@@ -28,23 +38,31 @@ classes = strrep(classes, '0', 'not_shifted');
 classes = strrep(classes, '1', 'shifted');
 
 % Feature names
-no_features = length(features);
-feature_names = strcat({'interval'}, cellstr(num2str((1:no_features)')));
+no_features_subset = size(features_subset, 2);
+feature_names = strcat({'interval'}, cellstr(num2str((1:no_features_subset)')));
 feature_names{length(feature_names)+1} = 'class';
 
 % Split into training set and test set
-train = [num2cell(features), classes];
+train = [num2cell(features_subset), classes];
 
-% We need samples with all possible classes in the testset, otherwise
-% randomforest fails. Hence we append the trainingset and testset
-test = [train ; train(TEST_ID, :)];
 train(TEST_ID, :) = [];
 
-train_weka = matlab2weka('shift-train', feature_names, train, length(train));
+train_weka = matlab2weka('shift-train', feature_names, train, size(train, 2));
 
 classifier = trainWekaClassifier(train_weka, CLASSIFIER);
 
+test_features = features(TEST_ID, :);
+
 for i=0:no_features
+    
+    % Shift test sample 1 interval to the left
+    test_features = circshift(test_features, [0, -1]);
+    
+    test_features_subset = test_features(:, FEATURES);
+    
+    % We need samples with all possible classes in the testset, otherwise
+    % randomforest fails. Hence we append the trainingset and testset
+    test = [train ; num2cell(test_features_subset) classes(TEST_ID)];
     
     test_weka = matlab2weka('shift-test', feature_names, test);
     
@@ -56,9 +74,6 @@ for i=0:no_features
         display(predicted);
         break;
     end
-    
-    % Shift test sample 1 interval to the left
-    test = [ circshift(test(:,1:end-1),[0,-1]) test(:,end)];
     
 end
 
