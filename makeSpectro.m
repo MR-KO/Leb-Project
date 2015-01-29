6667c6fe76f3d72ebd8c55fef6b1bdc3c0869668
YSturkenboom <Yuri Sturkenboom>
2015-01-29 15:49:25 +0100

    ASDFPANNENKOEK

---
+++ makeSpectro.m
@@ -0,0 +1,9 @@
+function makeSpectro(audio, Fs)
+    spectro = spectrogram(audio,128,120,128,8000,'yaxis');
+    speccy = surf(abs(spectro));
+    set(speccy, 'linestyle', 'none');
+    title('Spectogram of bird chirp');
+    xlabel('sample number (rate: 8k/sec)');
+    ylabel('frequency (Hz)');
+    zlabel('power');
+end
